4d4bb6de0aa001b20f494cd83af486f798a368dd
dmejias08 <85594834+dmejias08@users.noreply.github.com>
2021-10-13 18:58:12 -0600

    infixtoPosfix
    Testing the method found some errors
    -Count each number twice and the parenthesis as a operator

--- src/com/img/Main.java
+++ src/com/img/Main.java
@@ -1,17 +1,71 @@
 package com.img;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-        Client client1 = new Client();
-        Client client2 = new Client();
-        Client client3 = new Client();
-        Thread threadClient1 = new Thread((Runnable) client1);
-        Thread threadClient2 = new Thread((Runnable) client2);
-        Thread threadClient3 = new Thread((Runnable) client3);
+        String test = "(5+4)";
+        System.out.println(infixToPosfix(test));
+    }
 
-        threadClient1.start();
-        threadClient2.start();
-        threadClient3.start();
+
+    public static String infixToPosfix (String exp){
+
+        System.out.println("Estoy en infixtoPosfix");
+        String result = new String("");
+
+        // empty stack for operators
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++){
+            char ch = exp.charAt(i);
+
+            if (Character.isDigit(ch)){
+                result += ch;
+                System.out.println("Soy digit " + ch);
+
+            }else if (String.valueOf(ch) == "("){
+                operators.push(ch);
+                System.out.println("Soy (");
+
+                // pop until find "("
+            }else if (String.valueOf(ch) == ")"){
+                while (!operators.isEmpty() &&
+                        String.valueOf(operators.peek()) != "("){
+                    result += operators.pop();
+                    System.out.println("Soy )");
+                    operators.pop();
+
+                }
+            }else { // found an operator
+
+                while (!operators.isEmpty() &&
+                        Prec(String.valueOf(ch)) <= Prec(String.valueOf(operators.peek())))
+                {
+                    System.out.println("soy operator " + ch);
+                    result += String.valueOf(operators.pop());
+                }
+            }
+            operators.push(ch);
+        }
+        //pop all
+        while (operators.isEmpty()==true){
+            System.out.println("last while");
+            if(String.valueOf(operators.peek()) == "("){
+                return "Invalid";
+            }
+        }
+        System.out.println("Soy resutado "+result);
+        return result;
+    }
+    static int Prec (String operator){
+        if (operator == "+" || operator == "-"){
+            return 1;
+        }
+        else if (operator == "*" || operator == "/"){
+            return 2;
+        }
+        return -1;
     }
 }
